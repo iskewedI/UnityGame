5bb1df8b6b966302841834c68b823003842dd346
Ettore Bortolin <ettorej.bortolin@hotmail.com>
2021-05-24 15:59:11 -0300

    Turn enemy red when defeated

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -1,6 +1,4 @@
 using UnityEngine;
-using System.Threading.Tasks;
-using System;
 
 public class BulletController : MonoBehaviour
 {
--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
     [SerializeField]
     private float life = 10f;
-    
+
+    private MeshRenderer renderer;
+
+    private void Start()
+    {
+        renderer = GetComponent<MeshRenderer>();
+    }
     public void LoseLife(float damage)
     {
         life -= damage;
 
         if (life == 0)
         {
+            renderer.material.color = Color.red;
             Debug.Log("Morido");
         }
     }
