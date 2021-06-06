7f3ce5055243bd2eceeeadaff6280b614cdc2641
Ettore Bortolin <ettorej.bortolin@hotmail.com>
2021-06-06 18:15:12 -0300

    Add character rotation when walking

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,45 +3,67 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    private float WalkingSpeed = 4f;
+    private float WalkingSpeed = 2.4f;
     [SerializeField]
-    private float JumpForce = 5f;
+    private float RunningSpeed = 4f;
+    [SerializeField]
+    private float JumpForce = 5.6f;
+    [SerializeField]
+    private float MovementNormalizer = 0.025f;
 
-    private Rigidbody Physics;
+    private Rigidbody Rb;
     private bool IsJumping = false;
 
     // Start is called once at the beginning
     private void Start()
     {
-        Physics = GetComponent<Rigidbody>();
+        Rb = GetComponent<Rigidbody>();
     }
     // Update is called once per frame
     void Update()
     {
-        Walk(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-
         if (Input.GetButton("Jump") && !IsJumping)
         {
             Jump();
         }
     }
 
-    private void Walk(float horizontal, float vertical)
+    private void FixedUpdate()
     {
-        //transform.Translate(new Vector3(horizontal, 0f, vertical) * Time.deltaTime * WalkingSpeed);
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
 
+        //if (Input.GetKeyDown(KeyCode.LeftControl))
+        //{
+        //    MoveCharacter(Input.GetAxis("Horizontal") * Time.deltaTime, Input.GetAxis("Vertical") * Time.deltaTime, RunningSpeed);
+        //}
+
+        MoveCharacter(horizontal * Time.deltaTime, vertical * Time.deltaTime, WalkingSpeed);
+    }
+
+    private void MoveCharacter(float horizontal, float vertical, float speed)
+    {
         Vector3 forward = Camera.main.transform.forward;
         forward.y = 0;
         forward.Normalize();
+
         Vector3 right = Camera.main.transform.right;
         right.y = 0;
         right.Normalize();
-        transform.position += horizontal * Time.deltaTime * WalkingSpeed * right + vertical * Time.deltaTime * WalkingSpeed * forward;
+
+        transform.position += horizontal * speed * right + vertical * speed * forward;
+
+        if (horizontal != 0 || vertical != 0)
+        {
+            Vector3 lookRotation = new Vector3(horizontal, 0, vertical);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(lookRotation.normalized), MovementNormalizer);
+        }
     }
 
     private void Jump()
     {
-        Physics.AddForce(new Vector3(0, JumpForce, 0), ForceMode.Impulse);
+        Rb.AddForce(new Vector3(0, JumpForce, 0), ForceMode.Impulse);
         IsJumping = true;
     }
 
