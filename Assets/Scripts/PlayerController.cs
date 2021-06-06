467fe3ff5a6abc983cd4935eb86d1080e2bb35c3
Ettore Bortolin <ettorej.bortolin@hotmail.com>
2021-06-06 15:36:43 -0300

    Improve player camera

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    private float Speed = 1f;
+    private float WalkingSpeed = 4f;
     [SerializeField]
-    private float JumpForce = 1f;
+    private float JumpForce = 5f;
 
     private Rigidbody Physics;
     private bool IsJumping = false;
@@ -18,16 +18,31 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-
-        transform.Translate(new Vector3(horizontal, 0f, vertical) * Time.deltaTime * Speed);
+        Walk(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
         if (Input.GetButton("Jump") && !IsJumping)
         {
-            Physics.AddForce(new Vector3(0, JumpForce, 0), ForceMode.Impulse);
-            IsJumping = true;
+            Jump();
         }
+    }
+
+    private void Walk(float horizontal, float vertical)
+    {
+        //transform.Translate(new Vector3(horizontal, 0f, vertical) * Time.deltaTime * WalkingSpeed);
+
+        Vector3 forward = Camera.main.transform.forward;
+        forward.y = 0;
+        forward.Normalize();
+        Vector3 right = Camera.main.transform.right;
+        right.y = 0;
+        right.Normalize();
+        transform.position += horizontal * Time.deltaTime * WalkingSpeed * right + vertical * Time.deltaTime * WalkingSpeed * forward;
+    }
+
+    private void Jump()
+    {
+        Physics.AddForce(new Vector3(0, JumpForce, 0), ForceMode.Impulse);
+        IsJumping = true;
     }
 
     private void OnCollisionEnter(Collision collision)
