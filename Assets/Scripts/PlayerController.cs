f4b87ffaecf4946bd037e1698febeaa25e3204a3
Ettore Bortolin <ettorej.bortolin@hotmail.com>
2021-06-09 08:31:07 -0300

    Add comments

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,12 +14,13 @@ public class PlayerController : MonoBehaviour
     private Rigidbody Rb;
     private bool IsJumping = false;
 
-    // Start is called once at the beginning
+    // Start es llamado una única vez, al inicio de la ejecución
     private void Start()
     {
         Rb = GetComponent<Rigidbody>();
     }
-    // Update is called once per frame
+
+    // Update es llamado una vez por frame, de forma continua
     void Update()
     {
         if (Input.GetButton("Jump") && !IsJumping)
@@ -28,10 +29,15 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    // FixedUpdate se ejecuta sincrónicamente con el physics engine, siendo independiente de la cantidad de fps que pueda renderizar el equipo
     private void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+
+        /* TODO: intenté implementar que si se presiona el left shift, que el personaje corriera, me parece que tengo que especificar que tienen que presionarse
+                 tanto el left shift como los botones de direccionamiento. Me parece que lo que ocurre es que espera a que solo se presione el left shift para
+                 activarse, pero si el left shift se presiona sólo, los valores de vertical y horizontal son 0, por lo que no habría nada de movimiento */
 
         //if (Input.GetKeyDown(KeyCode.LeftControl))
         //{
@@ -43,6 +49,10 @@ public class PlayerController : MonoBehaviour
 
     private void MoveCharacter(float horizontal, float vertical, float speed)
     {
+        /* TODO: Normalize hace que un vector tenga distancia de 1, manteniendo su mismo sentido de dirección
+           (se supone facilita los cálculos con vectores, investigar qué tan cierto es eso en Unity) */
+
+        // TODO: probar con valores independientes de la cámara
         Vector3 forward = Camera.main.transform.forward;
         forward.y = 0;
         forward.Normalize();
@@ -51,12 +61,19 @@ public class PlayerController : MonoBehaviour
         right.y = 0;
         right.Normalize();
 
+        // Se mueve el personaje en la dirección y velocidad indicadas
         transform.position += horizontal * speed * right + vertical * speed * forward;
+
+        /* Me fijo si horizontal o vertical son diferentes a 0, porque si me muevo constantemente en base a esos valores
+           va a ocurrir que cuando el jugador deje de moverse, esos valores retornen a cero y mi personaje rote a su posición inicial
+           en vez de mantenerse viendo al lado al que apuntó por última vez la caminata indicada por el usuario */
 
         if (horizontal != 0 || vertical != 0)
         {
+            // Obtengo el sentido de la rotación de mi personaje en base al input de movimiento recibido por parte del usuario
             Vector3 lookRotation = new Vector3(horizontal, 0, vertical);
 
+            // Cambio la rotación en base al dato anterior, de la manera más "smooth" posible (tiene que haber una mejor forma de hacerlo)
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(lookRotation.normalized), MovementNormalizer);
         }
     }
