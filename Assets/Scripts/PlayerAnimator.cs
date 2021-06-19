8ee17c88ae9518bc368345a08698091e8fb201da
iskewedi <jtornello2019@hotmail.com>
2021-06-19 13:55:19 -0300

    Improved movement (NOTE: Disabled Jumping and Camera rotation)

--- Assets/Scripts/PlayerAnimator.cs
+++ Assets/Scripts/PlayerAnimator.cs
@@ -26,23 +26,38 @@ public class PlayerAnimator : MonoBehaviour
             // 1 seg despues = attacking = false;
         }
 
-        if (Player.IsIdle)
-        {
-            SpeedPercent = 0;
-            Debug.Log("Player is standing still.");
-        }
-        else if (!Player.IsRunning)
-        {
-            SpeedPercent = 0.5f;
-            Debug.Log("Player is walking.");
-        }
-        else
-        {
-            SpeedPercent = 1f;
-            Debug.Log("Player is running.");
-        }
+        SetAnimations();
+
+        // if (Player.IsIdle)
+        // {
+        //     SpeedPercent = 0;
+        //     Debug.Log("Player is standing still.");
+        // }
+        // else if (!Player.IsRunning)
+        // {
+        //     SpeedPercent = 0.5f;
+        //     Debug.Log("Player is walking.");
+        // }
+        // else
+        // {
+        //     SpeedPercent = 1f;
+        //     Debug.Log("Player is running.");
+        // }
 
         // Se le asigna el valor de nuestra variable al par�metro del animador, y le a�adimos el resto para que se vea m�s fluido el movimiento.
-        Animator.SetFloat("SpeedPercent", SpeedPercent, SmoothAnimation, Time.deltaTime);
+        // Animator.SetFloat("SpeedPercent", SpeedPercent, SmoothAnimation, Time.deltaTime);
+    }
+
+    private void SetAnimations()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Animator.SetFloat("InputHorizontal", horizontal, 0.0f, Time.deltaTime * 2f);
+        Animator.SetFloat("InputVertical", vertical, 0.0f, Time.deltaTime * 2f);
+
+        SpeedPercent = new Vector2(horizontal, vertical).sqrMagnitude;
+
+        Animator.SetFloat("SpeedPercent", SpeedPercent, 0.0f, Time.deltaTime);
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,24 +2,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float rotationSpeed = 0.05f;
     [SerializeField] private float MovementNormalizer = 0.025f;
     [SerializeField] private float WalkingSpeed = 4f;
     [SerializeField] private float RunningSpeed = 4f;
-
-
     [SerializeField] private float JumpForce = 5.6f;
 
-    private Rigidbody Rb;
-    // Hice p�blicos estos campos porque importo el PlayerController en PlayerAnimation y utilizo estos campos ah�
+    private Vector3 movementDirection;
+    // private Rigidbody Rb;
+    private CharacterController controller;
+    private Camera mainCamera;
+
+    // Hice publicos estos campos porque importo el PlayerController en PlayerAnimation y utilizo estos campos ahi
     public bool IsIdle = true;
-    public bool IsRunning => Input.GetKey(KeyCode.LeftShift); // GetKey Pregunta si el bot�n se mantiene presionado
+    public bool IsRunning => Input.GetKey(KeyCode.LeftShift); // GetKey Pregunta si el boton se mantiene presionado
     public bool IsJumping = false;
     public bool isAttacking = false;
 
-    // Start es llamado una �nica vez, al inicio de la ejecuci�n
+    // Start es llamado una unica vez, al inicio de la ejecucion
     private void Start()
     {
-        Rb = GetComponent<Rigidbody>();
+        // Rb = GetComponent<Rigidbody>();
+        mainCamera = Camera.main;
+        controller = GetComponent<CharacterController>();
     }
 
     // Update es llamado una vez por frame, de forma continua
@@ -27,70 +32,82 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetButton("Jump") && !IsJumping)
         {
-            Jump();
+            // Jump();
         }
     }
 
-    // FixedUpdate se ejecuta sincr�nicamente con el physics engine, siendo independiente de la cantidad de fps que pueda renderizar el equipo
+    // FixedUpdate se ejecuta sincronamente con el physics engine, siendo independiente de la cantidad de FPS que pueda renderizar el equipo
     private void FixedUpdate()
+    {
+
+        MoveCharacter();
+        /* Me fijo si horizontal o vertical son diferentes a 0, porque si me muevo constantemente en base a esos valores
+        va a ocurrir que cuando el jugador deje de moverse, esos valores retornen a cero y mi personaje rote a su posicion inicial
+        en vez de mantenerse viendo al lado al que apunto por ultima vez la caminata indicada por el usuario */
+        // if (horizontal != 0 || vertical != 0)
+        // {
+        // Identificamos si el jugador esta corriendo, guardamos el hecho en una variable por si la llegaramos a necesitar en otros casos
+        // if (IsRunning)
+        // {
+        //     MoveCharacter(horizontal * Time.deltaTime, vertical * Time.deltaTime, RunningSpeed);
+        // }
+        // else
+        // {
+        //     MoveCharacter(horizontal * Time.deltaTime, vertical * Time.deltaTime, WalkingSpeed);
+        // }
+
+        // IsIdle = false;
+        // }
+        // else
+        // {
+        //     IsIdle = true;
+        // }
+    }
+
+    private void MoveCharacter()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        /* Me fijo si horizontal o vertical son diferentes a 0, porque si me muevo constantemente en base a esos valores
-        va a ocurrir que cuando el jugador deje de moverse, esos valores retornen a cero y mi personaje rote a su posici�n inicial
-        en vez de mantenerse viendo al lado al que apunt� por �ltima vez la caminata indicada por el usuario */
-        if (horizontal != 0 || vertical != 0)
-        {
-            // Identificamos si el jugador est� corriendo, guardamos el hecho en una variable por si la llegaramos a necesitar en otros casos
-            if (IsRunning)
-            {
-                MoveCharacter(horizontal * Time.deltaTime, vertical * Time.deltaTime, RunningSpeed);
-                IsIdle = false;
-            }
-            else
-            {
-                MoveCharacter(horizontal * Time.deltaTime, vertical * Time.deltaTime, WalkingSpeed);
-                IsIdle = false;
-            }
-        }
-        else
-        {
-            IsIdle = true;
-        }
+        if (new Vector3(horizontal, vertical).sqrMagnitude <= 0) return;
+
+        float speed = IsRunning ? RunningSpeed : WalkingSpeed;
+
+        // Normalize convierte los vectores en vectores unitarios (reduce la magnitud del vector a 1, el dato del sentido es igual)
+        Vector3 forwardLook = mainCamera.transform.forward;
+        forwardLook.y = 0f;
+        forwardLook.Normalize();
+
+        Vector3 rightLook = mainCamera.transform.right;
+        rightLook.y = 0f;
+        rightLook.Normalize();
+
+        movementDirection = forwardLook * vertical + rightLook * horizontal;
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movementDirection), rotationSpeed);
+
+        controller.Move(movementDirection * Time.deltaTime * speed);
+        // Se mueve el personaje en la direccion y velocidad indicadas
+        // transform.position += horizontal * speed * right + vertical * speed * forward;
+
+        // Obtengo el sentido de la rotacion de mi personaje en base al input de movimiento recibido por parte del usuario
+        // Vector3 lookRotation = new Vector3(horizontal, 0, vertical);
+
+        // Cambio la rotacion en base al dato anterior, de la manera mas "smooth" posible (tiene que haber una mejor forma de hacerlo)
+        // transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(lookRotation.normalized), MovementNormalizer);
     }
 
-    private void MoveCharacter(float horizontal, float vertical, float speed)
-    {
-        Vector3 forward = Camera.main.transform.forward;
-        forward.y = 0;
-        forward.Normalize(); // Normalize convierte los vectores en vectores unitarios (reduce la magnitud del vector a 1, el dato del sentido es igual)
+    // private void Jump()
+    // {
+    //     Rb.AddForce(new Vector3(0, JumpForce, 0), ForceMode.Impulse);
+    //     IsJumping = true;
+    // }
 
-        Vector3 right = Camera.main.transform.right;
-        right.y = 0;
-        right.Normalize();
-
-        // Se mueve el personaje en la direcci�n y velocidad indicadas
-        transform.position += horizontal * speed * right + vertical * speed * forward;
-
-        // Obtengo el sentido de la rotaci�n de mi personaje en base al input de movimiento recibido por parte del usuario
-        Vector3 lookRotation = new Vector3(horizontal, 0, vertical);
-
-        // Cambio la rotaci�n en base al dato anterior, de la manera m�s "smooth" posible (tiene que haber una mejor forma de hacerlo)
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(lookRotation.normalized), MovementNormalizer);
-    }
-
-    private void Jump()
-    {
-        Rb.AddForce(new Vector3(0, JumpForce, 0), ForceMode.Impulse);
-        IsJumping = true;
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Floor"))
-        {
-            IsJumping = false;
-        }
-    }
+    // private void OnCollisionEnter(Collision collision)
+    // {
+    //     if (collision.gameObject.CompareTag("Floor"))
+    //     {
+    //         IsJumping = false;
+    //     }
+    // }
 }
